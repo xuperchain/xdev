0b1fe937f1625ff451656933abd92bc448deed87
chenfengjin <1871653365@qq.coom>
2021-06-01 12:58:31 +0800

    Add submodule support when build package

--- cmd/build.go
+++ cmd/build.go
@@ -44,7 +44,7 @@ type buildCommand struct {
 	output            string
 	compiler          string
 	makeFlags         string
-	modules           []string
+	submodules        []string
 }
 
 func newBuildCommand() *cobra.Command {
@@ -61,7 +61,7 @@ func newBuildCommand() *cobra.Command {
 	cmd.Flags().StringVarP(&c.output, "output", "o", "", "output file name")
 	cmd.Flags().StringVarP(&c.compiler, "compiler", "", "docker", "compiler env docker|host")
 	cmd.Flags().StringVarP(&c.makeFlags, "mkflags", "", "", "extra flags passing to make command")
-	cmd.Flags().StringSliceVarP(&c.modules, "submodule", "s", nil, "submodules")
+	cmd.Flags().StringSliceVarP(&c.submodules, "submodule", "s", nil, "build submodules")
 	return cmd
 }
 
@@ -75,10 +75,10 @@ func (c *buildCommand) parsePackage(root, xcache, xroot string) error {
 	if err != nil {
 		return err
 	}
-	if c.modules != nil {
+	if c.submodules != nil {
 		addons = append(addons, mkfile.DependencyDesc{
 			Name:    "self",
-			Modules: c.modules,
+			Modules: c.submodules,
 		})
 	}
 
